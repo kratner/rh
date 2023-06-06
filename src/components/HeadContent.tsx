60817d65059f3b6bb59e005cf27f2b5597544240
Keith Ratner <keith.ratner@gmail.com>
2023-06-05 19:11:39 -0700

    Viewport and styling

--- src/components/HeadContent.tsx
+++ src/components/HeadContent.tsx
@@ -18,6 +18,7 @@ const HeadContent: React.FC<HeadContentProps> = ({
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="keywords" content={keywords} />
       <link rel="icon" type="image/png" href={favicon} />
     </Helmet>
